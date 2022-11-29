5fe1c68470494862eafcd55fd13441629deecf59
Saad Al-Mridha <salmridha44@gmail.com>
2022-11-28 19:34:34 -0800

    class names for buttons changed to more meaningful names. Dark theme colours added to textarea as well

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,16 @@
 //When the “Dark Theme” button is clicked, the background colors of the sidebar and the container of the textarea and the buttons should change to darker colors, and the text on the button should be updated to “Light Theme”. Pressing the “Light Theme” button would reverse all the changes. (3 points)
 //Hint: Think about how you can do this by toggling a class property
 const body = document.querySelector("body")
-const darkThemeButton = document.querySelector(".b2")
+const darkThemeButton = document.querySelector(".DarkThemeButton")
 const aside = document.querySelector("aside")
+const textarea = document.querySelector("textarea")
+
 function darkTheme()
 {
     body.classList.toggle("darkBody")
     aside.classList.toggle("darkAside")
+    textarea.classList.toggle("darkTextarea")
+
 }
 darkThemeButton.addEventListener("click", darkTheme)
 
