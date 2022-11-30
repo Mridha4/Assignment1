217d216584c1ba8292d630a01f3523eefd92c168
Saad Al-Mridha <salmridha44@gmail.com>
2022-11-29 16:22:08 -0800

    created a function that searches for the title of the notes and pastes the body in the textarea

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ const textarea = document.querySelector("textarea");
 const newNoteButton = document.querySelector(".NewNoteButton");
 const saveButton = document.querySelector(".SaveButton");
 const cancelButton = document.querySelector(".CancelButton");
+const asideNotes = document.querySelector(".notes");
 const array_textarea = [textarea, saveButton, cancelButton];
 function darkTheme()
 {
@@ -54,14 +55,14 @@ newNoteButton.addEventListener("click", eraseText);
 
 //notesArray
 notesArray = [];
-//{title:"note one", body:"this is my first note"}
-// Function gives a prompt to the user
+// Function gives a prompt to the user and takes the input of the prompt and adds it to the sidebar
 function createNote()
 {
     let text = prompt("What would you like to name this note?");
     if (text != null) {
         notesArray.push({title: text, body: textarea.value});
     }
+    //creates a new list then adds it to the notes class (the entire ul)
     let list = document.createElement("li");
     let notes = document.querySelector(".notes");
     list.appendChild(document.createTextNode(text));
@@ -69,3 +70,14 @@ function createNote()
 }
 saveButton.addEventListener("click", createNote);
 
+
+// Function searches for the title of the notes and pastes the body in the textarea
+function savedNotes(e)
+{
+    for (items of notesArray){
+        if (items["title"] === e.target.textContent) {
+            textarea.value = items["body"];
+        }
+    }
+}
+asideNotes.addEventListener("click", savedNotes);
